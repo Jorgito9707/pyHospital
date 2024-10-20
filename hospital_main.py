036a79a60c879706b81d7d69b1206f8d13070d71
Jorge Valdés <jorgeluisvaldespineda@gmail.com>
2024-10-20 23:04:56 +0200

    Este es el punto de entrada principal para la aplicacion

---
+++ hospital_main.py
@@ -0,0 +1,11 @@
+from hospital.main import main
+
+"""
+    -Este es el punto de entrada principal del programa.
+    -Se llama a la función principal 'main' que se encuentra en el módulo 'hospital.main'.
+    -Se inicializa la base de datos y crea la interfaz gráfica.
+
+"""
+
+if __name__ == "__main__":
+    main()
